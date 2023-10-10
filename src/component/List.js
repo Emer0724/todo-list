6950b640f1a4cfa0814fc9fea4ed27041f0a7649
Leo Hu <innocence88226@gmail.com>
2023-10-10 11:29:19 +0800

    10/10

--- src/component/List.js
+++ src/component/List.js
@@ -1,11 +1,11 @@
-import React,{useState} from 'react'
+import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import styles from './List.module.css'
 
 export default function List(props) {
-  const {task,deleteTask, updated}=props
-  const {content, done}= task
+  const {task,deleteTask, updated}=props //傳入task單項任務資料、刪除任務、變更完成狀態的function、
+  const {content, done}= task //解構
   console.log(`${content}:${done}`);
   return (
     <>
@@ -16,7 +16,7 @@ export default function List(props) {
       checked={done} //讓checkbox與該任務的done狀態連動
       onChange={()=>{
         const updatedTask={...task, done: !done}
-        updated(updatedTask);
+        updated(updatedTask); //變更checkbox狀態來連動變更任務狀態的function
       }}
        />
       <div 
